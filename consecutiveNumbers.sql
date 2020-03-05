9174fc99df2291c3bff3dfc2458be534bcfa380a
MLSaj <60438701+MLSaj@users.noreply.github.com>
2020-03-05 12:57:40 -0500

    Add files via upload

---
+++ consecutiveNumbers.sql
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/consecutive-numbers/submissions/
+# Write your MySQL query statement below
+Select DISTINCT 
+l1.num AS ConsecutiveNums
+FROM Logs l1,
+    Logs l2,
+    Logs l3
+WHERE l1.id = l2.id - 1
+AND l2.id = l3.id - 1
+AND l1.num = l2.num
+AND l2.num = l3.num
