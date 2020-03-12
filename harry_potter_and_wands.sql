64f7d512e2ce43460651d0e6d72a4c48d283d101
MLSaj <60438701+MLSaj@users.noreply.github.com>
2020-03-11 21:50:53 -0400

    Update harry_potter_and_wands.sql

--- harry_potter_and_wands.sql
+++ harry_potter_and_wands.sql
@@ -4,10 +4,15 @@ Enter your query here.
 
 #https://www.hackerrank.com/challenges/harry-potter-and-wands/problem
 
-SELECT w.id, wp.age, w.coins_needed, w.power
-FROM Wands AS w JOIN Wands_Property AS wp on (w.code = wp.code)
-WHERE wp.is_evil=0 AND w.coins_needed = (SELECT min(coins_needed) 
-                                         FROM Wands AS w1 JOIN Wands_Property AS wp1 ON (w1.code = wp1.code)
-                                         GROUP BY w1.power, wp1.age
-                                         HAVING w1.power = w.power AND wp1.age = wp.age) 
-ORDER BY w.power DESC, wp.age DESC
+SELECT temp2.I, temp2.A, temp2.WNN, temp2.P FROM
+
+(SELECT MIN(W1.COINS_NEEDED) AS WN, WP1.AGE as AG, W1.POWER AS PW FROM WANDS W1 INNER JOIN WANDS_PROPERTY WP1 ON W1.CODE=WP1.CODE 
+ GROUP BY W1.POWER, WP1.AGE ORDER BY W1.POWER DESC, WP1.AGE DESC) temp1
+ 
+INNER JOIN
+
+(SELECT W.ID AS I, MIN(W.COINS_NEEDED) AS WNN, WP.AGE as A, W.POWER AS P  FROM WANDS W INNER JOIN WANDS_PROPERTY WP ON W.CODE=WP.CODE 
+WHERE WP.IS_EVIL=0
+GROUP BY W.POWER, WP.AGE, W.ID ORDER BY W.POWER DESC, WP.AGE DESC) temp2
+
+ON temp1.WN=temp2.WNN AND temp1.PW=temp2.P AND temp1.AG=temp2.A;
